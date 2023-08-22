2773bcfabef7e6512d351b47db80ddcaf296c643
HRA <122253551+hmiddot@users.noreply.github.com>
2023-08-22 17:17:43 +0330

    Create comp.js

---
+++ comp.js
@@ -0,0 +1,21 @@
+for (let i = 0; i < document.getElementsByTagName('define').length; i++) {
+    let define = document.getElementsByTagName('define')[i];
+    define.style.display = 'none';
+    let codes = define.innerHTML;
+    let attrs = codes.match(/{([^}]+)}/g);
+    if (attrs) {
+        attrs = attrs.map(match => match.slice(1, -1));
+    }
+    for (let j = 0; j < document.getElementsByTagName(define.getAttribute('name')).length; j++) {
+        let newCodes = codes;
+        for (let k = 0; k < attrs.length; k++) {
+            let attrContent = document.getElementsByTagName(define.getAttribute('name'))[j].getAttribute(attrs[k].split('=')[0]);
+            if (attrContent == null) {
+                newCodes = newCodes.replaceAll('{' + attrs[k] + '}', attrs[k].split('=')[1]);
+            } else {
+                newCodes = newCodes.replaceAll('{' + attrs[k] + '}', attrContent);
+            }
+        }
+        document.getElementsByTagName(define.getAttribute('name'))[j].innerHTML = newCodes;
+    }
+}
